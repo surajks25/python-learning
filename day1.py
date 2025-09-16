38ab35602064baa349fc6a23419b5633220ab7a7
surajks <surajkskulal@gmail.com>
2025-09-16 19:50:30 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,13 @@
+print("hello")
+value = "20"
+print(type(value))
+
+number1 = 20
+number2 = 30
+print(number1+number2)
+
+name = input("enter the value")
+print(name)
+value = int(input("enter the number"))
+print(type(value))
+
