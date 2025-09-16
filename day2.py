44ee8389765bd134f60750a7885c42dc5fe6f7fc
surajks <surajkskulal@gmail.com>
2025-09-16 22:59:47 +0530

    day2

---
+++ day2.py
@@ -0,0 +1,62 @@
+# from collections import Counter
+# str1 = "this is a string"
+# print(str1)
+# print(len(str1))
+# print(str1[2])
+
+# #slicing
+
+# print(str1[2:5])
+# print(str1[-7:-1])
+
+# print(str1.endswith("ing"))     #true or false
+# print(str1.capitalize())
+# replace = str1.replace("this", "this one")
+# print(replace)
+# print(str1.find("i"))
+# print(str1.count("i"))
+# print(Counter(str1))
+
+
+
+# age = 20
+# if (age > 20):
+#     print("above")
+# elif (age == 20):
+#     print("equal")
+
+# input = int(input("enter the age : "))
+# if(input >= 20):
+#     print("haii")
+# elif(input == 10):
+#     print("hello")
+
+
+
+
+# input = int(input("enter the age : "))
+# if(input % 2 == 0):
+#     print("even")
+# elif():
+#     print("odd")
+
+
+
+# input1 = int(input("Enter the val1: "))
+# input2 = int(input("Enter the val2: "))
+# input3 = int(input("Enter the val3: "))
+
+# if (input1 >= input2 and input1 >= input3):
+#     print("largest:", input1)
+# elif (input2 >= input3):
+#     print("largest:", input2)
+# else:
+#     print("largest:", input3)
+
+
+input = int(input("enter the value"))
+
+if(input % 5 == 0 ):
+    print("multiple of 5")
+else:
+    print("not a multiple of 5")
