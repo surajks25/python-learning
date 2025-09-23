fcf34f5ffebc8adde3236069ae56835e3def1043
surajks <surajkskulal@gmail.com>
2025-09-23 22:15:31 +0530

    day4

--- day4.py
+++ day4.py
@@ -87,3 +87,92 @@ print(std.get("name"))
 val = {"kannda":"30", "name":"jan"}
 std.update(val)
 print(std)
+
+#set in python
+
+# set is  a collection of unordered iteams each element set must be unique & immutable
+collection = {1,5,3,4,"xello", "aam"}
+print(collection)
+print(type(collection))
+print(len(collection))
+collection1 = set()
+print(collection1)
+
+#empty set also possible but not this type collection={}, this type collection = set()
+
+coll = set()
+coll.add(1)
+coll.add(2)
+coll.add(4)
+coll.add("haii")
+coll.add((1,2,3,4))
+# coll.add([1,2,3]) this is not possiblke becuase list is a mutable value,hash value will not match 
+print(coll)
+
+coll.remove(1)
+print(coll)
+
+print(len(coll))
+coll.clear()
+print(len(coll))
+
+#pop the value  random vaue will remove the set
+setValues = {"apnacollege","suraj","codevita","node","react"}
+print(setValues.pop() )
+
+#union in set
+set1 = {1,2,3}
+set2 = {3,5,6}
+print(set1.union(set2))
+print(set1.intersection(set2))
+
+#activity
+dictionary = {
+    "cats" : "a small animal",
+    "table" : ["a peace of furniture", "list of facts and figures"]
+}
+
+print(dictionary)
+
+#find the total classroom req each sub
+setDic = {
+    "python", "java", "c++", "javascript", "java",
+    "python", "java", "c++", "c"
+}
+
+print(len(setDic))
+
+#input
+
+# value1 =int(input("enter the valu1 "))
+# value2 =int(input("enter the valu2 "))
+# value3 =int(input("enter the valu3 "))
+
+# dictVal = {
+#     "chem" : value1,
+#     "sci" : value2,
+#     "math" : value3
+#     }
+# print(dictVal)
+
+# marks = {}
+
+# value1 =int(input("enter the valu1 "))
+# marks.update({"chem" : value1})
+# value2 =int(input("enter the valu2 "))
+# marks.update({"sc" : value2})
+# value3 =int(input("enter the valu3 "))
+# marks.update({"math" : value3})
+
+# print(marks)
+
+#we can save value same
+values = {9, 9.0 } 
+# it will show only 9 coz python will detech both are same thats why we save as like this string formate one one value
+values = {9, "9.0" } 
+# another way 
+valuess = {
+    "int" , 6,
+    "flot" , 6.5
+}
+print(valuess)
